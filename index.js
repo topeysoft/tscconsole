3c5cf526fcbc8478be619e5a96cafc2659c04d24
Temi Adeyeri <temi.adeyeri@gmail.com>
2017-04-14 19:43:58 -0500

    code update

---
+++ index.js
@@ -0,0 +1,23 @@
+var clc = require("cli-color");
+function init(){
+    var mapping = {
+    log: clc.blue,
+    warn: clc.yellow,
+    error: clc.red,
+    info: clc.green
+};
+["log", "warn", "error","info"].forEach(function(method) {
+    var oldMethod = console[method].bind(console);
+    console[method] = function() {
+        oldMethod.apply(
+            console,
+            [mapping[method](method.toUpperCase()+"  "+new Date().toISOString())]
+                .concat(arguments)
+        );
+    };   
+});
+}
+
+exports.init = init;
+
+
